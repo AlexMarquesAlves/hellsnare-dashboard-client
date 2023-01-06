534bc76b8ab7dd890d30f3ce18a5f702b3c1dd90
¡Duque <atanaelleonardo@gmail.com>
2023-01-06 16:24:35 -0300

    𝑺𝒆𝒕𝒕𝒊𝒏𝒈 𝒖𝒑 𝑬𝒔𝒍𝒊𝒏𝒕 𝑷𝒓𝒆𝒕𝒕𝒊𝒆𝒓

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mode =useSelector((state)=>state.global.mode;
+  const theme =useMemo(()=>createTheme(themeSettings(mode)),[mode]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
     </div>
   )
 }
