a69ab6579588cc6b69d472c7e753d958cde127f8
¡Duque <atanaelleonardo@gmail.com>
2023-01-06 15:55:20 -0300

    ...

--- src/templates/App/app.test.jsx
+++
@@ -1,8 +0,0 @@
-import { screen } from '@testing-library/react';
-import { App } from '.';
-import { renderTheme } from '../../styles/render-theme';
-
-it('should render heading', () => {
-  renderTheme(<App>Test</App>);
-  expect(screen.getByRole('heading', { name: 'Test' })).toMatchSnapshot();
-});
